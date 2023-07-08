b2e5660f21277eae90a9e88d2b791e96027734ae
myusername <andonyc2@icloud.com>
2023-07-07 20:10:56 -0600

    2 commit

--- src/main/java/com/Proyecto_web/Proyecto/ProyectoApplication.java
+++ src/main/java/com/Proyecto_web/Proyecto/ProyectoApplication.java
@@ -1,4 +1,4 @@
-package com.Proyecto_web.Proyecto;
+ package com.Proyecto_web.Proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
